18290e11eb628403b0062ba0c35f629d9c353d17
Charlaxy <charlaxy@gmail.com>
2014-06-30 00:37:31 -0500

    Started work on application.

---
+++ QRkey/Form.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QRkey
+{
+    public class Form : System.Windows.Forms.Form
+    {
+        public Form()
+            : base()
+        {
+            Menustrip = new MenuStrip();
+
+            {
+                var menuItem = new ToolStripMenuItem("File");
+                Menustrip.Items.Add(menuItem);
+                {
+                    var subMenuItem = new ToolStripMenuItem("Exit");
+                    subMenuItem.Click += (sender, e) => Application.Exit();
+                    menuItem.DropDown.Items.Add(subMenuItem);
+                }
+            }
+
+            Controls.Add(Menustrip);
+            Text = "QRkey";
+        }
+
+        public MenuStrip Menustrip
+        {
+            get;
+            private set;
+        }
+    }
+}
