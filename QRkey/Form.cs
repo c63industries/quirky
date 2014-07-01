69e9711f385537a1e7a6b27fb871008c38721baf
Charlaxy <charlaxy@gmail.com>
2014-07-01 13:31:16 -0500

    Added File -> Open Keys and Open XML

--- QRkey/Form.cs
+++ QRkey/Form.cs
@@ -18,8 +18,42 @@ namespace QRkey
             {
                 var menuItem = new ToolStripMenuItem("File");
                 Menustrip.Items.Add(menuItem);
+
                 {
-                    var subMenuItem = new ToolStripMenuItem("Open");
+                    var subMenuItem = new ToolStripMenuItem("Open Keys");
+                    subMenuItem.Click += (object sender, System.EventArgs e) =>
+                    {
+                        Stream myStream = null;
+                        OpenFileDialog openFileDialog = new OpenFileDialog();
+                        openFileDialog.InitialDirectory = "c:\\";
+                        openFileDialog.Filter = "csv files (*.csv)|*.csv|txt files (*.txt)|*txt";
+                        openFileDialog.FilterIndex = 2;
+                        openFileDialog.RestoreDirectory = true;
+
+                        if (openFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            try
+                            {
+                                if ((myStream = openFileDialog.OpenFile()) != null)
+                                {
+                                    using (myStream)
+                                    {
+                                        string path = openFileDialog.FileName;
+                                        string[] readText = File.ReadAllLines(path);
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                            }
+                        }
+                    };
+                    menuItem.DropDown.Items.Add(subMenuItem);
+                }
+
+                {
+                    var subMenuItem = new ToolStripMenuItem("Open XML");
                     subMenuItem.Click += (object sender, System.EventArgs e) =>
                     {
                         Stream myStream = null;
