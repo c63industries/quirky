93dd23acdcca45bafc51bbe434a31f9f1371b557
Charlaxy <charlaxy@gmail.com>
2014-07-03 21:22:19 -0500

    Added Xml attribute checks.

--- QRkey/Generator.cs
+++ QRkey/Generator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
@@ -32,11 +33,34 @@ namespace QRkey
             {
                 return;
             }
+
+            var x = xmlNode.Attributes["x"];
+            var y = xmlNode.Attributes["y"];
+
             switch (xmlNode.Name.ToUpper())
             {
                 case "IMAGE":
+                    var xmlAttribute = xmlNode.Attributes["file"];
+                    if (xmlAttribute == null)
+                    {
+                        return;
+                    }
+                    string file = xmlAttribute.Value;
+                    if (String.IsNullOrWhiteSpace(file))
+                    {
+                        return;
+                    }
+                    if (File.Exists(file))
+                    {
+                        return;
+                    }
                     break;
                 case "TEXT":
+                    var innerText = xmlNode.InnerText;
+                    if (String.IsNullOrWhiteSpace(innerText))
+                    {
+                        return;
+                    }
                     break;
             }
 
